81fc4f4c1262eefe9634c0eb6d6aade0f78d6c06
JangoBrick <mail@jangobrick.de>
2017-02-19 11:44:29 +0100

    Add methods for direct project property access

--- src/main/java/scratchlib/project/ScratchProject.java
+++ src/main/java/scratchlib/project/ScratchProject.java
@@ -2,12 +2,18 @@ package scratchlib.project;
 
 import java.io.ByteArrayOutputStream;
 import java.io.IOException;
+import java.util.Map.Entry;
 
 import scratchlib.objects.ScratchObject;
 import scratchlib.objects.ScratchObjectStore;
+import scratchlib.objects.fixed.collections.ScratchObjectAbstractDictionary;
 import scratchlib.objects.fixed.collections.ScratchObjectDictionary;
+import scratchlib.objects.fixed.data.ScratchObjectAbstractString;
 import scratchlib.objects.fixed.data.ScratchObjectString;
 import scratchlib.objects.fixed.data.ScratchObjectUtf8;
+import scratchlib.objects.fixed.forms.ScratchObjectColorForm;
+import scratchlib.objects.fixed.forms.ScratchObjectForm;
+import scratchlib.objects.inline.ScratchObjectBoolean;
 import scratchlib.objects.user.morphs.ScratchObjectStageMorph;
 import scratchlib.writer.ScratchOutputStream;
 
@@ -18,6 +24,60 @@ import scratchlib.writer.ScratchOutputStream;
  */
 public class ScratchProject
 {
+    /**
+     * Specifies the operating system (OS) version the project was created on.
+     * The default is "NT". Has no influence on the project.
+     */
+    public static final String INFO_OS_VERSION = "os-version";
+    /**
+     * Specifies the operating system (OS) platform the project was created on.
+     * The default is "Win32". Has no influence on the project.
+     */
+    public static final String INFO_PLATFORM = "platform";
+    /**
+     * Specifies the language of the project. The default is "en".
+     */
+    public static final String INFO_LANGUAGE = "language";
+    /**
+     * Specifies the project's save history. The default is "\r" (empty
+     * history). Has no influence on the project.
+     */
+    public static final String INFO_HISTORY = "history";
+    /**
+     * Specifies the program version the project was created with. This can be
+     * ANY STRING and has no influence on the project. The default is set
+     * according to {@link ScratchVersion#getVersionString()}.
+     */
+    public static final String INFO_SCRATCH_VERSION = "scratch-version";
+    /**
+     * Specifies the project's thumbnail. This is a
+     * {@link ScratchObjectColorForm}. Passing NIL is not recommended, since it
+     * crashes the Scratch file browser; the default is to omit it entirely.
+     */
+    public static final String INFO_THUMBNAIL = "thumbnail";
+    /**
+     * Specifies the project's comment. This is visible in the Scratch file
+     * browser, and through "File" -> "Project Notes". The default is the URL to
+     * this library's GitHub repository.
+     */
+    public static final String INFO_COMMENT = "comment";
+    /**
+     * Specifies the project's author. This is visible in the Scratch file
+     * browser. The default is "".
+     */
+    public static final String INFO_AUTHOR = "author";
+    /**
+     * Specifies the {@link ScratchObjectForm} storing the pen trails and stamps
+     * drawn by sprites. The default is NIL.
+     */
+    public static final String INFO_PEN_TRAILS = "penTrails";
+    /**
+     * BYOB 3.1.1 only. Specifies whether sprites have to stay on the stage
+     * (true; the default) or are allowed off stage (false). For Scratch, this
+     * is always true.
+     */
+    public static final String INFO_KEEP_ON_STAGE = "keepOnStage";
+
     private final ScratchVersion version;
     private ScratchObjectStore info = new ScratchObjectStore(ScratchObject.NIL);
     private ScratchObjectStore stage = new ScratchObjectStore(
@@ -34,23 +94,26 @@ public class ScratchProject
 
         ScratchObjectDictionary dict = new ScratchObjectDictionary();
         {
-            dict.put(new ScratchObjectString("os-version"),
+            dict.put(new ScratchObjectString(INFO_OS_VERSION),
                     new ScratchObjectString("NT"));
-            dict.put(new ScratchObjectString("platform"),
+            dict.put(new ScratchObjectString(INFO_PLATFORM),
                     new ScratchObjectString("Win32"));
-            dict.put(new ScratchObjectString("language"),
+            dict.put(new ScratchObjectString(INFO_LANGUAGE),
                     new ScratchObjectString("en"));
-            dict.put(new ScratchObjectString("history"),
+            dict.put(new ScratchObjectString(INFO_HISTORY),
                     new ScratchObjectUtf8("\r"));
-            dict.put(new ScratchObjectString("scratch-version"),
+            dict.put(new ScratchObjectString(INFO_SCRATCH_VERSION),
                     new ScratchObjectString(version.getVersionString()));
             // thumbnail omitted (-> ColorForm)
-            dict.put(new ScratchObjectString("comment"), new ScratchObjectUtf8(
-                    "https://github.com/JangoBrick/scratchlib"));
-            dict.put(new ScratchObjectString("author"),
+            dict.put(new ScratchObjectString(INFO_COMMENT),
+                    new ScratchObjectUtf8(
+                            "https://github.com/JangoBrick/scratchlib"));
+            dict.put(new ScratchObjectString(INFO_AUTHOR),
                     new ScratchObjectUtf8(""));
-            // keepOnStage omitted (-> Boolean)
-            // penTrails omitted (-> Form)
+            dict.put(new ScratchObjectString(INFO_PEN_TRAILS),
+                    ScratchObject.NIL);
+            dict.put(new ScratchObjectString(INFO_KEEP_ON_STAGE),
+                    ScratchObjectBoolean.TRUE);
         }
         info.set(dict);
 
@@ -103,6 +166,60 @@ public class ScratchProject
     public void setStageSection(ScratchObjectStore stage)
     {
         this.stage = stage;
+    }
+
+    /**
+     * Finds the given property in this project's info dictionary.
+     * 
+     * @param key The property's key.
+     * @return The associated value.
+     */
+    public ScratchObject getInfoProperty(String key)
+    {
+        Entry<ScratchObject, ScratchObject> e = findInfoEntry(key);
+        return e != null ? e.getValue() : null;
+    }
+
+    /**
+     * Associates the given value with the given property name in this project's
+     * info dictionary.
+     * 
+     * @param key The property's key.
+     * @param value The value to associate with the key.
+     */
+    public void setInfoProperty(String key, ScratchObject value)
+    {
+        Entry<ScratchObject, ScratchObject> e = findInfoEntry(key);
+
+        ScratchObject k = e != null ? e.getKey() : new ScratchObjectString(key);
+        ((ScratchObjectAbstractDictionary) info.get()).put(k, value);
+    }
+
+    /**
+     * Finds the info dictionary entry for the given key string. If no such
+     * entry exists, returns {@code null}.
+     * 
+     * @param key The entry's key.
+     * @return The entry.
+     */
+    private Entry<ScratchObject, ScratchObject> findInfoEntry(String key)
+    {
+        ScratchObject i = this.info.get();
+        ScratchObjectAbstractDictionary d = (ScratchObjectAbstractDictionary) i;
+
+        for (Entry<ScratchObject, ScratchObject> e : d.entrySet()) {
+            ScratchObject k = e.getKey();
+            if (!(k instanceof ScratchObjectAbstractString)) {
+                continue;
+            }
+            ScratchObjectAbstractString ks = (ScratchObjectAbstractString) k;
+            if (!ks.getValue().equals(key)) {
+                continue;
+            }
+            return e;
+        }
+
+        return null;
     }
 
     /**
