fee0963868cf8dbd24f9397319f0aa710c469d23
Fabian Meyer <3982806+meyfa@users.noreply.github.com>
2018-01-26 19:25:24 +0100

    Add unit tests for ScratchNumbers

---
+++ src/test/java/scratchlib/util/ScratchNumbersTest.java
@@ -0,0 +1,92 @@
+package scratchlib.util;
+
+import static org.hamcrest.CoreMatchers.instanceOf;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertThat;
+
+import java.math.BigInteger;
+
+import org.junit.Test;
+
+import scratchlib.objects.inline.ScratchObjectAbstractNumber;
+import scratchlib.objects.inline.ScratchObjectFloat;
+import scratchlib.objects.inline.ScratchObjectLargeNegativeInteger;
+import scratchlib.objects.inline.ScratchObjectLargePositiveInteger;
+import scratchlib.objects.inline.ScratchObjectSmallInteger;
+import scratchlib.objects.inline.ScratchObjectSmallInteger16;
+
+
+public class ScratchNumbersTest
+{
+    private void assertNumber(Class<? extends ScratchObjectAbstractNumber> type,
+            double value, ScratchObjectAbstractNumber actual)
+    {
+        assertThat(actual, instanceOf(type));
+        assertEquals(value, actual.doubleValue(), 0.00000001);
+    }
+
+    @Test
+    public void ofDouble()
+    {
+        assertNumber(ScratchObjectSmallInteger16.class, 0,
+                ScratchNumbers.of(0.0));
+        assertNumber(ScratchObjectSmallInteger16.class, 0,
+                ScratchNumbers.of(-0.0));
+
+        assertNumber(ScratchObjectSmallInteger16.class, 10,
+                ScratchNumbers.of(10.0));
+        assertNumber(ScratchObjectSmallInteger16.class, -10,
+                ScratchNumbers.of(-10.0));
+
+        assertNumber(ScratchObjectSmallInteger.class, 1_000_000,
+                ScratchNumbers.of(1_000_000.0));
+        assertNumber(ScratchObjectSmallInteger.class, -1_000_000,
+                ScratchNumbers.of(-1_000_000.0));
+
+        assertNumber(ScratchObjectLargePositiveInteger.class, 10_000_000_000L,
+                ScratchNumbers.of(10_000_000_000.0));
+        assertNumber(ScratchObjectLargeNegativeInteger.class, -10_000_000_000L,
+                ScratchNumbers.of(-10_000_000_000.0));
+
+        assertNumber(ScratchObjectFloat.class, 1.2345,
+                ScratchNumbers.of(1.2345));
+        assertNumber(ScratchObjectFloat.class, -1.2345,
+                ScratchNumbers.of(-1.2345));
+    }
+
+    @Test
+    public void ofLong()
+    {
+        assertNumber(ScratchObjectSmallInteger16.class, 0,
+                ScratchNumbers.of(0L));
+        assertNumber(ScratchObjectSmallInteger16.class, 0,
+                ScratchNumbers.of(-0L));
+
+        assertNumber(ScratchObjectSmallInteger16.class, 10,
+                ScratchNumbers.of(10L));
+        assertNumber(ScratchObjectSmallInteger16.class, -10,
+                ScratchNumbers.of(-10L));
+
+        assertNumber(ScratchObjectSmallInteger.class, 1_000_000,
+                ScratchNumbers.of(1_000_000L));
+        assertNumber(ScratchObjectSmallInteger.class, -1_000_000,
+                ScratchNumbers.of(-1_000_000L));
+
+        assertNumber(ScratchObjectLargePositiveInteger.class, 10_000_000_000L,
+                ScratchNumbers.of(10_000_000_000L));
+        assertNumber(ScratchObjectLargeNegativeInteger.class, -10_000_000_000L,
+                ScratchNumbers.of(-10_000_000_000L));
+    }
+
+    @Test
+    public void ofBigInteger()
+    {
+        assertNumber(ScratchObjectLargePositiveInteger.class, 0,
+                ScratchNumbers.of(new BigInteger("0")));
+
+        assertNumber(ScratchObjectLargePositiveInteger.class, 1000,
+                ScratchNumbers.of(new BigInteger("1000")));
+        assertNumber(ScratchObjectLargeNegativeInteger.class, -1000,
+                ScratchNumbers.of(new BigInteger("-1000")));
+    }
+}
