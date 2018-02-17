a3278559e6a40ab8aec8c1257d698e51d8f4e95c
Fabian Meyer <3982806+meyfa@users.noreply.github.com>
2018-02-17 12:03:11 +0100

    Add unit tests for ScratchReader

---
+++ src/test/java/scratchlib/reader/ScratchReaderTest.java
@@ -0,0 +1,50 @@
+package scratchlib.reader;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotNull;
+
+import java.io.File;
+import java.io.IOException;
+
+import org.junit.Test;
+
+import scratchlib.objects.ScratchObject;
+import scratchlib.objects.fixed.data.ScratchObjectAbstractString;
+import scratchlib.project.ScratchProject;
+import scratchlib.project.ScratchVersion;
+
+
+public class ScratchReaderTest
+{
+    @Test
+    public void readsFile() throws IOException
+    {
+        ScratchReader obj = new ScratchReader();
+
+        final File file = new File("./src/test/resources/empty.sb");
+        ScratchProject proj = obj.read(file);
+
+        assertNotNull(proj);
+        assertEquals(ScratchVersion.SCRATCH14, proj.getVersion());
+
+        ScratchObject c = proj.getInfoProperty(ScratchProject.INFO_COMMENT);
+        assertEquals("test project",
+                ((ScratchObjectAbstractString) c).getValue());
+    }
+
+    @Test
+    public void readsInputStream() throws IOException
+    {
+        ScratchReader obj = new ScratchReader();
+
+        ScratchProject proj = obj.read(
+                getClass().getClassLoader().getResourceAsStream("empty.sb"));
+
+        assertNotNull(proj);
+        assertEquals(ScratchVersion.SCRATCH14, proj.getVersion());
+
+        ScratchObject c = proj.getInfoProperty(ScratchProject.INFO_COMMENT);
+        assertEquals("test project",
+                ((ScratchObjectAbstractString) c).getValue());
+    }
+}
