55db01167332840a99eceb32e78aa90602d2c571
Fabian Meyer <3982806+meyfa@users.noreply.github.com>
2020-08-02 12:44:13 +0200

    Remove unused imports

--- src/main/java/scratchlib/objects/user/morphs/ScratchObjectScriptableMorph.java
+++ src/main/java/scratchlib/objects/user/morphs/ScratchObjectScriptableMorph.java
@@ -4,7 +4,6 @@ import java.awt.image.BufferedImage;
 import java.util.Arrays;
 import java.util.Collections;
 import java.util.stream.Stream;
-import java.util.stream.StreamSupport;
 
 import scratchlib.media.ScratchFormEncoder;
 import scratchlib.objects.ScratchObject;
--- src/test/java/scratchlib/objects/ScratchObjectStoreTest.java
+++ src/test/java/scratchlib/objects/ScratchObjectStoreTest.java
@@ -4,7 +4,6 @@ import java.io.ByteArrayInputStream;
 import java.io.ByteArrayOutputStream;
 import java.io.IOException;
 import java.nio.charset.StandardCharsets;
-import java.util.Arrays;
 import java.util.Collections;
 
 import org.junit.jupiter.api.Test;
